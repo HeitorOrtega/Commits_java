8a88013146096db049bd4d143586452a24947ed5
MarcosVini222 <lourencmarcos@gmail.com>
2025-08-15 21:20:42 -0300

    Arrumando

--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.spring_mvc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<LivroRepository, Long> {
-}
